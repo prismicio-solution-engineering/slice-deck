bcea5698e1750486b6a0b43bdb2a1a09bbc085ef
Dmsooja <dev@sooja.fr>
2024-07-09 15:50:22 +0200

    Add headings

--- slices/slides/Offer/index.tsx
+++ slices/slides/Offer/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Container } from "@/components/Slides/Container";
 import { OfferSlice } from "@/prismicio-types";
 import { Content, isFilled } from "@prismicio/client";
@@ -8,6 +9,7 @@ import { LeftCol, RightCol } from "@/components/Slides/Columns";
 import Table from "@/components/Table";
 import { SlideFullWidth } from "@/components/Slides/SlideFullWidth";
 import { GlobalPrismicRichText } from "@/components/GlobalPrismicRichText";
+import { Headings } from "@/components/Slides/Headings";
 
 /**
  * Props for `Offer`.
@@ -30,57 +32,92 @@ const Offer = ({
       settings={context.settings}
       theme={slice.primary.theme}
     >
-      {slice.variation === "default" ?
+      <Headings
+        eyebrow={slice.primary.eyebrow}
+        title={slice.primary.title}
+        alignTop
+        titleSize="!text-4xl"
+      />
+      {slice.variation === "default" ? (
         <SlideTwoCols larger="right" className="mb-0">
-          <LeftCol>toto</LeftCol>
+          <LeftCol>
+            <GlobalPrismicRichText field={slice.primary.description} />
+          </LeftCol>
           <RightCol>
             {slice.primary.tables.map((table, index) => {
-              return isFilled.linkToMedia(table.table_csv) &&
-                <div key={index}>
-                  <Table tableCsv={table.table_csv} hasHeader={table.table_has_header} />
-                </div>
-            })
-            }
+              return (
+                isFilled.linkToMedia(table.table_csv) && (
+                  <div key={index}>
+                    <Table
+                      tableCsv={table.table_csv}
+                      hasHeader={table.table_has_header}
+                    />
+                  </div>
+                )
+              );
+            })}
           </RightCol>
         </SlideTwoCols>
-        :
-        slice.variation === "fullWidthTables" ?
-          <SlideFullWidth className="mb-0">
-            {slice.primary.tables.map((table,index) => {
-              return isFilled.linkToMedia(table.table_csv) &&
-                <div key={index}>
-                  <GlobalPrismicRichText field={table.title} />
-                  <Table tableCsv={table.table_csv} hasHeader={table.table_has_header} />
-                </div>
-            })
-            }
-          </SlideFullWidth>
-          : slice.variation === "twoTablesColumns" &&
+      ) : slice.variation === "fullWidthTables" ? (
+        <SlideFullWidth className="mb-0">
+          {slice.primary.tables.map((table, index) => {
+            return (
+              <div key={index}>
+                <GlobalPrismicRichText field={table.title} />
+                {isFilled.linkToMedia(table.table_csv) && (
+                  <Table
+                    tableCsv={table.table_csv}
+                    hasHeader={table.table_has_header}
+                  />
+                )}
+              </div>
+            );
+          })}
+        </SlideFullWidth>
+      ) : (
+        slice.variation === "twoTablesColumns" && (
           <SlideTwoCols className="mb-0">
             <LeftCol>
               {slice.primary.content_left_column.map((table, index) => {
-                return isFilled.linkToMedia(table.table_csv) &&
+                return (
                   <div key={index}>
-                    <GlobalPrismicRichText field={table.title} />
+                    <GlobalPrismicRichText
+                      field={table.title}
+                      classNames="!text-3xl"
+                    />
                     <GlobalPrismicRichText field={table.description} />
-                    <Table tableCsv={table.table_csv} hasHeader={table.table_has_header} />
+                    {isFilled.linkToMedia(table.table_csv) && (
+                      <Table
+                        tableCsv={table.table_csv}
+                        hasHeader={table.table_has_header}
+                      />
+                    )}
                   </div>
-              })
-              }
+                );
+              })}
             </LeftCol>
             <RightCol>
-              {slice.primary.content_right_column.map((table,index) => {
-                return isFilled.linkToMedia(table.table_csv) &&
+              {slice.primary.content_right_column.map((table, index) => {
+                return (
                   <div key={index}>
-                    <GlobalPrismicRichText field={table.title} />
+                    <GlobalPrismicRichText
+                      field={table.title}
+                      classNames="!text-3xl"
+                    />
                     <GlobalPrismicRichText field={table.description} />
-                    <Table tableCsv={table.table_csv} hasHeader={table.table_has_header} />
+                    {isFilled.linkToMedia(table.table_csv) && (
+                      <Table
+                        tableCsv={table.table_csv}
+                        hasHeader={table.table_has_header}
+                      />
+                    )}
                   </div>
-              })
-              }
+                );
+              })}
             </RightCol>
           </SlideTwoCols>
-      }
+        )
+      )}
     </Container>
   );
 };
