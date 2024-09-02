210f759b0b7402ce1045bd83cdb61eb3d2dadd93
Dmsooja <dev@sooja.fr>
2024-09-02 11:44:52 +0200

    Remove diagram variation

--- slices/slides/ValueProposition/index.tsx
+++ slices/slides/ValueProposition/index.tsx
@@ -2,7 +2,6 @@ import { ValuePropositionSlice } from "@/prismicio-types";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import TwoCols from "./TwoCols";
-import TwoColsDiagram from "./TwoColsDiagram";
 import TwoColsLogos from "./TwoColsLogos";
 import List from "./List";
 import { Context } from "@/utils/GlobalTypes";
